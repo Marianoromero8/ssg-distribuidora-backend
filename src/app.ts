3fc1e777efe5d86e2c36c6aa2d8a6ec842d35be2
marianoromero8 <marianoromero97.mr@gmail.com>
2026-06-24 17:32:51 -0300

    CORS for frontend deploy update

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ import { env } from "./config/env";
 
 const app = express();
 
-const allowedOrigins = [env.FRONTEND_URL, "http://localhost:3000", "http://localhost:3002", "http://localhost:3003"];
+const allowedOrigins = [env.FRONTEND_URL, "http://localhost:3000", "http://localhost:3002", "http://localhost:3003", "https://admin-distribuidora.vercel.app"];
 
 app.use(helmet());
 app.use(
